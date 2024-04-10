6a8f92f079d63b724b77e6abf99e398c5791bb4b
Nikolay <w3d@MacBook-Pro-Nikolay.local>
2024-04-10 22:28:10 +0200

    finish app

--- Home.tsx
+++ Home.tsx
@@ -1,4 +1,7 @@
 import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
@@ -9,120 +12,164 @@ import React, { useState } from "react";
 
 import { Image } from "expo-image";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 const Home = () => {
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
 
   const [BMI, setBMI] = useState(0.0);
 
-  React.useEffect(() => {
-    //отрабатывает код внутри себя тогда когда изменяются значения в его квадратных скобках,
-    // есои там пусто - то он отрабатывает 1 раз в самом начале перед рендером экрана
-    setHeight("193");
-  }, []);
+  const isDisabled = height.length === 0 || weight.length === 0;
+
+  const blocks = [
+    {
+      style: styles.YellowBlock,
+      imgStyle: styles.YellowImage,
+      image: require("./assets/yellow.png"),
+      value: "Under 18",
+      note: "Under Weight",
+      condition: BMI <= 18.5 && BMI >= 1,
+    },
+    {
+      style: styles.GreenBlock,
+      imgStyle: styles.GreenImage,
+
+      image: require("./assets/green.png"),
+      value: "18-25",
+      note: "Healthy Weight",
+      condition: BMI > 18.5 && BMI < 25,
+    },
+    {
+      style: styles.RedBlock,
+      imgStyle: styles.GreenImage,
+
+      image: require("./assets/red.png"),
+      value: "Over 25",
+      note: "Overweight",
+      condition: BMI >= 25,
+    },
+  ];
 
   const handlerCalcBMI = () => {
     const currentWeight = parseInt(weight);
     const currentHeight = parseInt(height);
 
-    if (isNaN(currentWeight) || isNaN(currentHeight)) {
-      return;
-    }
-
     const heightMetrs = currentHeight / 100;
     setBMI(Number((currentWeight / (heightMetrs * heightMetrs)).toFixed(1)));
+    storeData();
   };
+
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem("height", height);
+      await AsyncStorage.setItem("weight", weight);
+
+      const jsonBMI = JSON.stringify(BMI);
+      await AsyncStorage.setItem("BMI", jsonBMI);
+    } catch (e) {
+      throw new Error("Error saving data");
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const height = await AsyncStorage.getItem("height");
+      const weight = await AsyncStorage.getItem("weight");
+      const jsonBMI = await AsyncStorage.getItem("BMI");
+
+      if (height && weight && jsonBMI) {
+        setHeight(height);
+        setWeight(weight);
+        setBMI(Number(JSON.parse(jsonBMI)) || 0);
+      }
+    } catch (e) {
+      throw new Error("error getting data");
+    }
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+
+  const filterDigits = (input: string): string => {
+    return input.replace(/\D/g, "");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>BMI Calculator</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          value={height}
-          onChangeText={(value) => {
-            setHeight(value);
-          }}
-          style={styles.input}
-          placeholder="Heigth-M"
-          placeholderTextColor={"rgba(90,90,190,1)"}
-          keyboardType="number-pad"
-        />
-        <TextInput
-          value={weight}
-          onChangeText={(value) => {
-            setWeight(value);
-          }}
-          style={styles.input}
-          placeholder="Weigth-KG"
-          placeholderTextColor={"rgba(90,90,190,1)"}
-          keyboardType="number-pad"
-        />
-      </View>
-
-      <TouchableOpacity
-        style={styles.goButton}
-        onPress={() => {
-          handlerCalcBMI();
-        }}
-      >
-        <Text style={styles.goButtonText}>Go</Text>
-      </TouchableOpacity>
-
-      <Text style={styles.BMIText}>{BMI}</Text>
-
-      <View style={styles.line} />
-
-      <View style={styles.ColorContainer}>
-        <View
-          style={[
-            styles.YellowBlock,
-            BMI <= 18.5 && BMI >= 1 && { opacity: 1 },
-          ]}
-        >
-          <Image
-            style={styles.YellowImage}
-            source={require("./assets/yellow.png")}
-            contentFit="fill"
+    <ScrollView
+      style={{ minHeight: "100%", backgroundColor: "rgba(50,50,50,1)" }}
+    >
+      <View style={styles.container}>
+        <Text style={styles.titleText}>BMI Calculator</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={height}
+            onChangeText={(value) => {
+              setHeight(filterDigits(value));
+            }}
+            style={styles.input}
+            placeholder="Heigth-M"
+            placeholderTextColor={"rgba(90,90,190,1)"}
+            keyboardType="number-pad"
           />
-          <Text style={styles.valueText}>Under 18</Text>
-          <Text style={styles.noteText}>Under Weight</Text>
+          <TextInput
+            value={weight}
+            onChangeText={(value) => {
+              setWeight(filterDigits(value));
+            }}
+            style={styles.input}
+            placeholder="Weigth-KG"
+            placeholderTextColor={"rgba(90,90,190,1)"}
+            keyboardType="number-pad"
+          />
         </View>
 
-        <View
-          style={[styles.GreenBlock, BMI > 18.5 && BMI < 25 && { opacity: 1 }]}
+        <TouchableOpacity
+          disabled={isDisabled}
+          style={[styles.goButton, isDisabled && { opacity: 0.5 }]}
+          onPress={() => {
+            handlerCalcBMI();
+          }}
         >
-          <Image
-            style={styles.GreenImage}
-            source={require("./assets/green.png")}
-            contentFit="fill"
-          />
-          <Text style={styles.valueText}>Under 18</Text>
-          <Text style={styles.noteText}>Under Weight</Text>
-        </View>
+          <Text style={styles.goButtonText}>Go</Text>
+        </TouchableOpacity>
 
-        <View style={[styles.RedBlock, BMI >= 25 && { opacity: 1 }]}>
-          <Image
-            style={styles.RedImage}
-            source={require("./assets/red.png")}
-            contentFit="fill"
-          />
-          <Text style={styles.valueText}>Under 18</Text>
-          <Text style={styles.noteText}>Under Weight</Text>
+        <Text style={styles.BMIText}>{BMI}</Text>
+
+        <View style={styles.line} />
+
+        <View style={styles.ColorContainer}>
+          {blocks.map((block, index) => (
+            <View
+              key={index}
+              style={[block.style, block.condition && { opacity: 1 }]}
+            >
+              <Image
+                style={block.imgStyle}
+                source={block.image}
+                contentFit="fill"
+              />
+              <Text style={styles.valueText}>{block.value}</Text>
+              <Text style={styles.noteText}>{block.note}</Text>
+            </View>
+          ))}
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgba(50,50,50,1)",
     alignItems: "center",
     justifyContent: "center",
   },
   titleText: {
     color: "rgba(180, 168, 45, 1)",
     fontSize: 25,
+    marginTop: 40,
   },
 
   inputContainer: { flexDirection: "row", gap: 50, marginTop: 50 },
